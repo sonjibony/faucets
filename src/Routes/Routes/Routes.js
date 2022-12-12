409535182d0b2a1a4a1fa4f9464c8c568a702039
sonjibony <sonjibony@gmail.com>
2022-12-12 20:29:03 +0600

    router setup done

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+{
+    
+}
+    ]
+    }
+])
